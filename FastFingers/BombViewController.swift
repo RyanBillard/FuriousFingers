5fb4190e0043574fb33158fce0504eb388fbff99
Niko Draca <nikola.draca@live.ca>
2017-01-29 09:14:40 -0500

    bomb stuff

--- FastFingers/BombViewController.swift
+++ FastFingers/BombViewController.swift
@@ -12,11 +12,12 @@ class BombViewController: UIViewController {
     
     var location = CGPoint(x: 0, y:0)
     var bombView = UIImageView()
+    var ticker = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
     
-//    override func createInstructionView() -> InstructionView {
-//        return InstructionView(title: "Tick Tock", instructions: "Slide the bomb off your screen to give it to a friend. Dont't be the last one holding onto it when it goes off!", participants: session.connectedPeers.map({ $0.displayName }))
-//    }
-
+    //    override func createInstructionView() -> InstructionView {
+    //        return InstructionView(title: "Tick Tock", instructions: "Slide the bomb off your screen to give it to a friend. Dont't be the last one holding onto it when it goes off!", participants: session.connectedPeers.map({ $0.displayName }))
+    //    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch: UITouch! = touches.first! as UITouch
         location = touch.location(in: self.view)
@@ -36,16 +37,16 @@ class BombViewController: UIViewController {
             bombView.removeFromSuperview()
             
             // broadcast bomb pass
-
+            
         }
-        
+            
         else if (location.x > 0.7*(UIScreen.main.bounds.width)) {
             UIView.animate(withDuration: 0.1, delay: 0, options: .curveLinear, animations: {
                 self.bombView.center.x = UIScreen.main.bounds.width + 200
             }, completion: nil)
             
             bombView.removeFromSuperview()
-
+            
         }
     }
     
@@ -59,14 +60,28 @@ class BombViewController: UIViewController {
         title.font = UIFont(name: "AvenirNext-Medium", size: 30)
         title.textAlignment = .center
         
+        ticker.backgroundColor = UIColor(red:1.00, green:0.77, blue:0.54, alpha:1.0)
+        
+        
         let bombImg = UIImage(named: "bomb")
         let bombView = UIImageView(image: bombImg)
         
         self.bombView = bombView
+        
+        self.view.addSubview(ticker)
         self.view.addSubview(self.bombView)
         self.view.addSubview(title)
         
+        
+        
         bombView.center = self.view.center
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 5, delay: 0, options: .curveLinear, animations: {
+            self.ticker.center.y = UIScreen.main.bounds.height + self.ticker.frame.height / 2
+        }, completion: nil)
+    }
 }
